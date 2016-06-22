2d4f77d37ae5f02bae6e69e4d5b0835b024be03c
Сергей Колибаба <kolisergej@MacBook-Pro-Sergej.local>
2016-06-22 22:19:41 +0300

    Check connection when transition to foreground

--- Actoo/AppDelegate.swift
+++ Actoo/AppDelegate.swift
@@ -33,6 +33,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        if !Reachability.isConnectedToNetwork() {
+            let viewController = window!.rootViewController
+            showErrorController(title: connectionError, message: checkInternetConnection + "\n Actoo will use saved data.", view: viewController!)
+        }
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
