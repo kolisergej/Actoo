4f9a8f6366dc7951c38bdd42a8d263a7c50a1e43
Kolibaba Sergey <kolisergej@gmail.com>
2016-07-02 17:21:13 +0300

    Rename title of word list

--- Actoo/WordsListViewController.swift
+++ Actoo/WordsListViewController.swift
@@ -21,7 +21,7 @@ class WordsListViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        navigationItem.title = "Word List"
+        navigationItem.title = "Dictionary"
         
         reminderViewController = appDelegate.window?.rootViewController?.childViewControllers[1].childViewControllers[0] as! ReminderViewController
     }
