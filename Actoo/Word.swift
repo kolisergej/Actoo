98f6904877c66103ae4560ab7d4cb74cb38fe194
Kolibaba Sergey <kolisergej@gmail.com>
2016-07-05 22:24:59 +0300

    Remove encode/decode code

--- Actoo/Word.swift
+++ Actoo/Word.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Word: NSObject, NSCoding {
+class Word: NSObject {
     let origWord: String
     let fromLng: String
     let trWord: String
@@ -25,25 +25,5 @@ class Word: NSObject, NSCoding {
         self.syns = syns
         self.examples = examples
         self.rating = rating
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        self.origWord = aDecoder.decodeObjectForKey("origWord") as! String
-        self.fromLng = aDecoder.decodeObjectForKey("fromLng") as! String
-        self.trWord = aDecoder.decodeObjectForKey("trWord") as! String
-        self.toLng = aDecoder.decodeObjectForKey("toLng") as! String
-        self.syns = aDecoder.decodeObjectForKey("syns") as! [String]
-        self.examples = aDecoder.decodeObjectForKey("examples") as! [String: String]
-        self.rating = aDecoder.decodeObjectForKey("rating") as! Int
-    }
-    
-    func encodeWithCoder(aCoder: NSCoder) {
-        aCoder.encodeObject(self.origWord, forKey: "origWord")
-        aCoder.encodeObject(self.fromLng, forKey: "fromLng")
-        aCoder.encodeObject(self.trWord, forKey: "trWord")
-        aCoder.encodeObject(self.toLng, forKey: "toLng")
-        aCoder.encodeObject(self.syns, forKey: "syns")
-        aCoder.encodeObject(self.examples, forKey: "examples")
-        aCoder.encodeObject(self.rating, forKey: "rating")
-    }
+    }    
 }
